3ae4a3e8b50fa4ea2f4ccf8484e6e9d0982fb4ae
BeratDizdar <139409706+BeratDizdar@users.noreply.github.com>
2026-07-21 08:42:53 +0300

    ufak deneyler

--- swl.h
+++ swl.h
@@ -1,4 +1,7 @@
 #pragma once
+// ufak deneyler için ekledim
+//#define APIENTRY __cdecl
+//#define WINGDIAPI
 
 void swl_CreateWindow(const char* t, int w, int h);
 void*swl_GetWindowPtr();
@@ -9,3 +12,7 @@ int  swl_IsKeyDown(int y);
 int  swl_IsKeyPressed(int y);
 int  swl_IsKeyReleased(int y);
 void swl_GetMousePos(int* x, int* y);
+void swl_GL_CreateContext();
+void swl_GL_DestroyContext();
+void swl_GL_SwapBuffers();
+void*swl_GL_GetProcAddress(const char* proc);
